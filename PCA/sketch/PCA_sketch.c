7753bb0997dfabd978c64cf8128bdf3250f5967c
Pavel Krobot <xkrobo01@stud.fit.vutbr.cz>
2013-07-04 17:10:23 +0200

    Added simple "sampling" - timebin division. Added basis for "hashing" of IP addresses and port numbers for more efficient entropy calculations.

--- PCA/sketch/PCA_sketch.c
+++ PCA/sketch/PCA_sketch.c
@@ -45,9 +45,11 @@
 #include <libtrap/trap.h>
 #include "../../unirec.h"
 
+#include "PCA_sketch.h"
+
 // Struct with information about module
 trap_module_info_t module_info = {
-   "Anomaly detection using PCA and sketch subspaces module.", // Module name
+   "Module for anomaly detection using PCA and sketch subspaces.",// Module name
    // Module description
    "This module detecting network anomalies in data flows.\n"
    ""
@@ -56,20 +58,10 @@ trap_module_info_t module_info = {
    "   Outputs: 2:\n"
    "     \t1.UniRec - information about time in witch an anomaly(-ies) "
                                                                   "occuring.\n"
-   "     \t2.UniRec - flows responsible for an anomaly(-ies)",
+   "     \t2.UniRec - flow(s) responsible for an anomaly(-ies)",
    1, // Number of input interfaces
    2, // Number of output interfaces
 };
-
-
-
-// Dummy structure used in this example
-/*
-typedef struct {
-   uint32_t foo;
-   uint32_t bar;
-} my_data_struct_t;
-*/
 
 static int stop = 0;
 
@@ -80,18 +72,45 @@ void signal_handler(int signal)
 {
    if (signal == SIGTERM || signal == SIGINT) {
       stop = 1;
-      trap_terminate(); // this interrupt a possible waiting in recv/send functions
+      trap_terminate();// this interrupt a possible waiting in recv/send functions
    }
 }
+
+/* !!!TODO - make it better...
+ * Dumb hash-IP-address functions for IPv4, IPv6 and port number
+ */
+uint16_t hash_ipv4(uint32_t address)
+{
+   return(address%SKETCH_WIDTH);
+}
+
+uint16_t hash_ipv6(uint64_t lower_half_of_address)
+{
+   return(lower_half_of_address%SKETCH_WIDTH);
+}
+
+uint16_t hash_port(uint16_t port_number)
+{
+   return(port_number%SKETCH_WIDTH);
+}
+/*
+ * END OF DUMMIES !!!TODO
+ */
 
 int main(int argc, char **argv)
 {
    int ret;
-   unsigned int simple_counter=0;
+   int i;
+   uint8_t new_timebin_flag=1;
+   uint32_t start_of_timebin=0;
+   uint32_t start_of_actual_flow;
+   uint32_t timebin_counter=0;
+   uint32_t sketches [SKETCH_WIDTH][NUMBER_OF_FEATURES*SKETCH_SIZE][NUMBER_OF_HASH_FUNCTION];
+   uint16_t ft_hash_values [NUMBER_OF_FEATURES];
    trap_ifc_spec_t ifc_spec;
-   
+
    // ***** TRAP initialization *****
-   
+
    // Let TRAP library parse command-line arguments and extract its parameters
    ret = trap_parse_params(&argc, argv, &ifc_spec);
    if (ret != TRAP_E_OK) {
@@ -99,65 +118,97 @@ int main(int argc, char **argv)
          trap_print_help(&module_info);
          return 0;
       }
-      fprintf(stderr, "ERROR in parsing of parameters for TRAP: %s\n", trap_last_error_msg);
+      fprintf(stderr, "ERROR in parsing of parameters for TRAP: %s\n",
+              trap_last_error_msg);
       return 1;
    }
-   
+
    // Initialize TRAP library (create and init all interfaces)
    ret = trap_init(&module_info, ifc_spec);
    if (ret != TRAP_E_OK) {
       fprintf(stderr, "ERROR in TRAP initialization: %s\n", trap_last_error_msg);
       return 2;
    }
-   
+
    // We don't need ifc_spec anymore, destroy it
    trap_free_ifc_spec(ifc_spec);
-   
+
    signal(SIGTERM, signal_handler);
    signal(SIGINT, signal_handler);
-   
+
    // ***** Main processing loop *****
-   
+
    // Read data from input, process them and write to output
    while (!stop) {
       const void *data_ptr;
       uint16_t data_size;
-      
+
       // Receive data from any input interface, wait until data are available
       ret = trap_get_data(TRAP_MASK_ALL, &data_ptr, &data_size, TRAP_WAIT);
       if (ret == TRAP_E_TERMINATED) {
          break; // Module was terminated while waiting for new data (e.g. by Ctrl-C)
       } else if (ret != TRAP_E_OK) {
          // Some error ocurred
-         fprintf(stderr, "Error: trap_get_data() returned %i (%s)\n", ret, trap_last_error_msg);
+         fprintf(stderr, "Error: trap_get_data() returned %i (%s)\n", ret,
+                 trap_last_error_msg);
          break;
       }
-      
-      // CAST THE DATA TO THE EXPECTED TYPE/STRUCT (but first check the size) 
-      if (data_size != sizeof(ur_basic_flow_t)) {
-         fprintf(stderr, "Error: data with wrong size received (expected size: %lu, received size: %hu)\n",
-                 sizeof(ur_basic_flow_t), data_size);
-         break;
-      }
-      const ur_basic_flow_t *flow_data = (ur_basic_flow_t *)data_ptr;
-      
-      // PROCESS THE DATA
-      ur_basic_flow_t new_data;
-      new_data.src_port = flow_data->src_port;
-      new_data.dst_port = flow_data->dst_port;
 
-      printf("%u Flow: srcPort %u / dstPort %u\n", ++simple_counter, new_data.src_port, new_data.dst_port);
+      // CAST THE DATA TO THE EXPECTED TYPE/STRUCT (but first check the size)
+      if (data_size != sizeof(ur_basic_flow_t)) {
+         fprintf(stderr, "Error: data with wrong size received (expected size:"
+                 "%lu, received size: %hu)\n",
+                 sizeof(ur_basic_flow_t),data_size);
+         break;
+      }
+      const ur_basic_flow_t *received_data = (ur_basic_flow_t *)data_ptr;
+
+      // PROCESS THE DATA
+      ur_basic_flow_t flow_data;
+      flow_data.src_addr = received_data->src_addr;
+      flow_data.dst_addr = received_data->dst_addr;
+      flow_data.src_port = received_data->src_port;
+      flow_data.dst_port = received_data->dst_port;
+      start_of_actual_flow = (received_data->first)>>32;
+
+      if(start_of_actual_flow>start_of_timebin+TIMEBIN_SIZE){
+         new_timebin_flag=1;
+      }
+      // !!!TODO overflow?
+      if (new_timebin_flag){
+         new_timebin_flag=0;
+         ++timebin_counter;
+         start_of_timebin=start_of_actual_flow;
+         printf("Start of %u. timebin in %u\n",timebin_counter,start_of_actual_flow);
+      }
+
+     /* if(ip_is4(&(flow_data.src_addr))){
+         ft_hash_values[FSRCIP]=hash_ipv4(ip_get_v4_as_int(&(flow_data.src_addr)));
+         ft_hash_values[FDSTIP]=hash_ipv4(ip_get_v4_as_int(&(flow_data.dst_addr)));
+      } else {
+         ft_hash_values[FSRCIP]=hash_ipv6(flow_data.src_addr.ui64[1]);
+         ft_hash_values[FDSTIP]=hash_ipv6(flow_data.dst_addr.ui64[1]);
+      }
+      ft_hash_values[FSRCPORT]=hash_port(flow_data.src_port);
+      ft_hash_values[FDSTPORT]=hash_port(flow_data.dst_port);
+
+      printf("%u : %u : %u : %u \n",ft_hash_values[0],ft_hash_values[1],ft_hash_values[2],ft_hash_values[3]);
+
+      for (i = 0; i < NUMBER_OF_HASH_FUNCTION; i++){
+         //hash it
+      }*/
+
       // Send data to interface 0, if ifc is not ready, wait at most 10ms
       //trap_send_data(0, (void*)&new_data, sizeof(my_data_struct_t), 10000);
-      sleep(1);
    }
-   
+
+//   printf("\nTotal flows count: %u\n",v4counter+v6counter);
+
    // ***** Cleanup *****
-   
+
    // Do all necessary cleanup before exiting
    // (close interfaces and free allocated memory)
    trap_finalize();
-   
+
    return 0;
 }
-
--- PCA/sketch/PCA_sketch.h
+++ PCA/sketch/PCA_sketch.h
@@ -2,8 +2,22 @@
 #define _PCA_SKETCH_H_
 
 #define SKETCH_SIZE 512
+#define SKETCH_WIDTH 4096 // For better performance it should be power of 2 since the value is used for modulus
+
+#define NUMBER_OF_FEATURES 4
 
 #define NUMBER_OF_HASH_FUNCTION 4
 
+#define TIMEBIN_SIZE 30 // Size of single timebin in seconds
+
+#define WORKING_TIMEBIN_WINDOW_SIZE 100
+
+enum features_order
+{
+   FSRCIP=0,
+   FSRCPORT,
+   FDSTIP,
+   FDSTPORT
+};
 
 #endif
