7c533d79bf441a957e7e02c4542ce039a3262d9a
Vrana Roman <xvrana20@benefizio.liberouter.org>
2013-07-18 15:09:09 +0200

    Added BSD License to files.

--- blacklistfilter/blacklistfilter.cpp
+++ blacklistfilter/blacklistfilter.cpp
@@ -1,9 +1,47 @@
 /**
  * \file spoofing.cpp
- * \brief IP spoofing detector for Nemea
+ * \brief Main module for IPBlackLIstDetector.
  * \author Roman Vrana, xvrana20@stud.fit.vutbr.cz
  * \date 2013
  */
+
+/*
+ * Copyright (C) 2013 CESNET
+ *
+ * LICENSE TERMS
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name of the Company nor the names of its contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * ALTERNATIVELY, provided that this notice is retained in full, this
+ * product may be distributed under the terms of the GNU General Public
+ * License (GPL) version 2 or later, in which case the provisions
+ * of the GPL apply INSTEAD OF those given above.
+ *
+ * This software is provided ``as is'', and any express or implied
+ * warranties, including, but not limited to, the implied warranties of
+ * merchantability and fitness for a particular purpose are disclaimed.
+ * In no event shall the company or contributors be liable for any
+ * direct, indirect, incidental, special, exemplary, or consequential
+ * damages (including, but not limited to, procurement of substitute
+ * goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether
+ * in contract, strict liability, or tort (including negligence or
+ * otherwise) arising in any way out of the use of this software, even
+ * if advised of the possibility of such damage.
+ *
+ */
+
 
 #include <map>
 #include <string>
@@ -689,7 +727,6 @@ int main (int argc, char** argv)
     // clean up before termination
 //    cout << count << " flows went through." << endl;
   //  cout << bl_count << " were marked." << endl;
-    cout << hash_blacklist.table_size << endl;
     ur_free_template(templ);
     ht_destroy(&hash_blacklist);
     trap_finalize();
