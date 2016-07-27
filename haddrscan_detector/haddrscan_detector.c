dd0c7da87eaa8168a2c85a0e002fea320ce4cc48
Marek Svepes <svepemar@fit.cvut.cz>
2016-07-27 15:19:45 +0200

    haddrscan_detector: bugfix: wrong name of unirec field for time_last time stamp

--- haddrscan_detector/haddrscan_detector.c
+++ haddrscan_detector/haddrscan_detector.c
@@ -282,7 +282,7 @@ int main(int argc, char **argv)
             goto cleanup;
          }
          ts_first = ur_get(in_tmplt, recv_data, F_TIME_FIRST);
-         ts_last = ur_get(in_tmplt, recv_data, F_TIME_FIRST);
+         ts_last = ur_get(in_tmplt, recv_data, F_TIME_LAST);
          np = (item_t *) new_item;
          if (np->addr_cnt == 0) {
             np->ts_first = ts_first;
