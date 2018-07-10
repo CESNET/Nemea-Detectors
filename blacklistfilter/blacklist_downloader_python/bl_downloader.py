8dd49fe95dba638c5ff427274a798d2fd59cc19d
Filip Suster <sustefil@fit.cvut.cz>
2018-07-10 14:42:18 +0200

    [blacklistfilter] Downloader refactoring; Fetching URL blacklists

--- blacklistfilter/blacklist_downloader_python/bl_downloader.py
+++ blacklistfilter/blacklist_downloader_python/bl_downloader.py
@@ -21,68 +21,12 @@ logger.setLevel(logging.DEBUG)
 
 ip_regex = re.compile('\\b((2(5[0-5]|[0-4][0-9])|[01]?[0-9][0-9]?)\.){3}(2(5[0-5]|[0-4][0-9])|[01]?[0-9][0-9]?)((/(3[012]|[12]?[0-9]))?)\\b')
 
-check_interval = 1 * 60  # Time period (secs) to check for blacklist changes
+# Just very simple regex to eliminate commentaries
+url_regex = re.compile('^[^#/].*\..+')
+
+check_interval = 1 * 60 # Time period (secs) to check for blacklist changes
 
 blacklists = []
-
-
-class Blacklist:
-    def __init__(self, bl_type_name, bl_type_id, bl):
-        self.type = bl_type_name
-        self.type_id = bl_type_id
-        self.id = bl.attrib['id']
-        self.entities = []
-        self.last_download = None
-
-        # Generate variables from the XML config file
-        for element in bl:
-            setattr(self, element.attrib['name'], element.text)
-
-    def __str__(self):
-        return str(self.__dict__)
-
-    def extract_ips(self, data):
-        extracted = []
-
-        for line in data.decode('utf-8').splitlines():
-            match = re.search(ip_regex, line)
-            if match:
-                extracted.append(match.group(0))
-
-        return extracted
-
-    def download_and_update(self):
-        try:
-            req = requests.get(self.source, timeout=10)
-
-            if req.status_code == 200:
-                data = req.content
-
-                new_entities = self.extract_ips(data)
-
-                if new_entities == self.entities:
-                    # Blacklist entites not changed since last download
-                    return 0
-
-                self.entities = new_entities
-
-                # filename = 'blacklists/{}_{}.blist'.format(self.type, self.id)
-                # with open(filename, 'w') as f:
-                #     f.write(data.decode())
-
-                logger.debug('Updated blacklist {}'.format(self.name))
-
-                return 1
-
-            else:
-                logger.warning('Couldnt fetch blacklist: {}\n'
-                               'Status code: {}'.format(self.source, req.status_code))
-
-        except requests.exceptions.Timeout:
-            logger.warning('Couldnt fetch blacklist: {}\n'
-                           'Timeout reached'.format(self.source))
-
-
 
 # Sorting comparator, splits the IP in format "A.B.C.D(/X),Y,Z"
 # into tuple of IP (A, B, C, D), which is comparable by python (numerically)
@@ -93,49 +37,156 @@ def split_ip(ip):
     else:
         ip = ip.split(',')[0]
 
+    try:
+        tuple_ip = tuple(int(part) for part in ip.split('.'))
+    except ValueError as e:
+        logger.warning('Couldnt sort this IP addr: {}'.format(ip))
+        logger.warning(e)
+        tuple_ip = (0, 0, 0, 0)
+
     """Split a IP address given as string into a 4-tuple of integers."""
-    return tuple(int(part) for part in ip.split('.'))
+    return tuple_ip
 
 
-def merge_and_sort():
-    all_entities = []
+class Blacklist:
+    def __init__(self, bl):
+        self.entities = []
+        self.last_download = None
 
-    # Enrich the entities with blacklist indexes (type id, id)
+        # Generate variables from the XML config file
+        for element in bl:
+            setattr(self, element.attrib['name'], element.text)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def download_and_update(self):
+        updated = False
+
+        try:
+            req = requests.get(self.source, timeout=10)
+
+            if req.status_code == 200:
+                data = req.content
+
+                new_entities = self.extract_entities(data)
+
+                if new_entities != self.entities:
+                    # Blacklist entities changed since last download
+                    self.entities = new_entities
+                    updated = True
+
+                    logger.debug('Updated blacklist {}'.format(self.name))
+
+            else:
+                logger.warning('Couldnt fetch blacklist: {}\n'
+                               'Status code: {}'.format(self.source, req.status_code))
+
+        except requests.RequestException as e:
+            logger.warning('Couldnt fetch blacklist: {}\n'
+                           '{}'.format(self.source, e))
+
+        return updated
+
+
+class IPBlacklist(Blacklist):
+    detector_file = 'bl_records_sorted_IP.txt'
+    separator = ','
+    comparator = split_ip
+
+    def __init__(self, bl):
+        super().__init__(bl)
+
+    def extract_entities(self, data):
+        extracted = []
+
+        for line in data.decode('utf-8').splitlines():
+            match = re.search(ip_regex, line)
+            if match:
+                extracted.append(match.group(0))
+
+        return extracted
+
+
+class URLBlacklist(Blacklist):
+    detector_file = 'bl_records_sorted_URL.txt'
+    separator = '\\'
+    comparator = str
+
+    def __init__(self, bl):
+        super().__init__(bl)
+
+    def extract_entities(self, data):
+        extracted = []
+
+        for line in data.decode('utf-8').splitlines():
+            match = re.search(url_regex, line)
+            if match:
+                url = match.group(0)
+                url = url.replace('https://', '', 1)
+                url = url.replace('http://', '', 1)
+                url = url.replace('www.', '', 1)
+                url = url.lower()
+                while url[-1] == '/':
+                    url = url[:-1]
+                # TODO: Maybe normalize also?
+                extracted.append(url)
+
+        return extracted
+
+
+def create_detector_file(bl_type: Blacklist):
+    all_entities = dict()
+
+    # Enrich the entities with blacklist index (bitfield way), merge the indexes if the same entity
+    # found on more blacklists
     for bl in blacklists:
-        enriched = [entity + ',{},{}'.format(bl.type_id, bl.id) for entity in bl.entities]
-        all_entities.extend(enriched)
+        bl_idx = 2 ** (int(bl.id) - 1)
+        for entity in bl.entities:
+            if entity in all_entities.keys() and all_entities[entity] & bl_idx == 0:
+                all_entities[entity] = all_entities[entity] + bl_idx
+            else:
+                all_entities[entity] = bl_idx
 
-    with open('../ipdetect/bl_records_sorted.txt'.format(time.time()), 'w') as f:
-        f.write('\n'.join(sorted(all_entities, key=split_ip)))
+    # Create sorted list of entities and their cumulative indexes
+    all_entities = sorted(['{entity}{sep}{idx}'.format(entity=entity, idx=idx, sep=bl_type.separator)
+                           for entity, idx in all_entities.items()],
+                           key=bl_type.comparator)
+
+    with open(bl_type.detector_file, 'w') as f:
+        f.write('\n'.join(all_entities))
 
 
 def parse_config():
-    tree = ET.parse("../ipdetect/bld_userConfigFile.xml")
+    tree = ET.parse("bld_userConfigFile.xml")
     bl_type_array = tree.getroot().getchildren()[0].getchildren()
 
     for bl_type in bl_type_array:
-        bl_type_name = bl_type.attrib['type']
-        bl_type_id = bl_type.attrib['type_id']
+        type = bl_type.attrib['type']
         for bl in bl_type:
-            blacklists.append(Blacklist(bl_type_name, bl_type_id, bl))
+            if type == "IP":
+                blacklists.append(IPBlacklist(bl))
+            elif type == "URL":
+                blacklists.append(URLBlacklist(bl))
 
 
 def run(s):
     # schedule next check immediately
     s.enter(check_interval, 1, run, (s,))
 
-    updated_count = 0
+    for bl_type in [IPBlacklist, URLBlacklist]:
+        updated = False
 
-    for bl in blacklists:
-        if bl.last_download + 60 * bl.download_interval < time.time():
-            updated_count += bl.download_and_update()
+        for bl in blacklists:
+            if isinstance(bl, bl_type):
+                # if bl.last_download and bl.last_download + 60 * bl.download_interval < time.time():
+                updated += bl.download_and_update()
 
-    if updated_count:
-        merge_and_sort()
-        logger.info('NEW IP Blacklist created')
-
-    else:
-        logger.info('Check for updates done, no changes')
+        if updated:
+            create_detector_file(bl_type)
+            logger.info('NEW {} Blacklist created'.format(bl_type.__name__))
+        else:
+            logger.info('Check for {} updates done, no changes'.format(bl_type.__name__))
 
 
 if __name__ == '__main__':
