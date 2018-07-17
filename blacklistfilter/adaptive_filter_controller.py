24a63e6bf9f212f9d10bb69ec4391f9aa72083a7
Filip Suster <sustefil@fit.cvut.cz>
2018-07-17 13:53:14 +0200

    [blacklistfilter] Adaptive filter WIP

--- blacklistfilter/adaptive_filter_controller.py
+++ blacklistfilter/adaptive_filter_controller.py
@@ -1,44 +1,92 @@
+#!/usr/bin/env python3
+
 import pytrap
 import sys
+from threading import Thread
 from queue import Queue
 
 IP_IF = 0
 URL_IF = 1
 
-def fetch_data():
-    pass
+
+class Receiver:
+    def __init__(self, input_ifcs, output_ifcs):
+        """
+        Trap initialization for input and output interfaces
+        """
+        self.trap = pytrap.TrapCtx()
+        self.trap.init(sys.argv, input_ifcs, output_ifcs)
+
+        # Set up required format to accept any unirec format.
+        self.trap.setRequiredFmt(IP_IF)     # Refers to basic (IP) flows from ipdetect
+        self.trap.setRequiredFmt(URL_IF)    # Refers to flows with HTTP headers from urldetec
+        self.trap.setVerboseLevel(0)
+
+        # Queue for received flows
+        self.queue = Queue()
+
+    def __create_threads__(self):
+        # Create workers for each receiver
+        self.ip_rcv = Thread(target=self.__fetch_data__, args=[IP_IF])
+        self.url_rcv = Thread(target=self.__fetch_data__, args=[URL_IF])
+
+    def run(self):
+        self.__create_threads__()
+
+        # Run multireciver
+        self.ip_rcv.start()
+        self.url_rcv.start()
+
+    def join_and_quit(self):
+        # Join the threads
+        self.ip_rcv.join()
+        self.url_rcv.join()
+        self.queue.put(None)
+
+        # Free allocated memory
+        self.trap.finalize()
+
+    def __fetch_data__(self, interface):
+        """
+        Fetches data from trap context and puts them to
+        queue as a IP/URL/DNS flow based on interface input (detector)
+        Arguments:
+        trap        pytrap.trapCtx
+        interface   int IP_IF/URL_IF
+        queue       Queue
+        """
+        while True:
+            try:
+                data = self.trap.recv(interface)
+            except pytrap.FormatChanged as e:
+                print('koko')
+                fmttype, inputspec = self.trap.getDataFmt(interface)
+                rec = pytrap.UnirecTemplate(inputspec)
+                data = e.data
+            if len(data) <= 1:
+                break
+
+            rec.setData(data)
+
+            print(rec.SRC_IP)
+            print(interface)
+            exit(3)
+
+            if interface is IP_IF:
+                pass
+            else:
+                pass
 
 
 
 
 if __name__ == '__main__':
     # Create a new trap context
-    trap = pytrap.TrapCtx()
-    """
-    Trap initialization for two input interfaces, and no output interface
-    """
-    trap.init(sys.argv, 2, 0)
-    # Set up required format to accept any unirec format.
-    trap.setRequiredFmt(IP_IF)     # Refers to basic (IP) flows from ipdetect
-    trap.setRequiredFmt(URL_IF)    # Refers to flows with HTTP headers from urldetec
-    trap.setVerboseLevel(0)
 
-    # Data queue
-    flow_queue = Queue()
-    # Create workers for each receiver
-    basic_rcv = Thread(target=fetch_data, args=(trap, BASIC_IF, flow_queue))
-    smtp_rcv = Thread(target=fetch_data, args=(trap, SMTP_IF, flow_queue))
+    receiver = Receiver(2, 0)
+    receiver.run()
+    receiver.join_and_quit()
+
     # Handle the received data from receivers
-    data_handler = Thread(target=data_handling, args=(detector, flow_queue))
-    # Run multireciver
-    basic_rcv.start()
-    smtp_rcv.start()
-    data_handler.start()
-    # Join the threads
-    basic_rcv.join()
-    smtp_rcv.join()
-    # Stop data_handler
-    flow_queue.put(None)
-    data_handler.join()
-    # Free allocated memory
-    trap.finalize()
+    # data_handler = Thread(target=data_handling, args=(detector, flow_queue))
+    # data_handler.start()
