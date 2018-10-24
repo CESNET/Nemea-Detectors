462b72b4f07d36d37495466f3699adc96f15a6a9
Filip Suster <sustefil@fit.cvut.cz>
2018-10-24 20:27:49 +0200

    blacklistfilter: watcher: Adapt for IPv6, Pure c++ compilation

--- blacklistfilter/blacklist_watcher.h
+++ blacklistfilter/blacklist_watcher.h
@@ -1,14 +1,11 @@
 #ifndef BLACKLISTFILTER_BLACKLIST_WATCHER_H
 #define BLACKLISTFILTER_BLACKLIST_WATCHER_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/**
+* Mutex for synchronization.
+*/
+static pthread_mutex_t BLD_SYNC_MUTEX;
 
 void *watch_blacklist_files(void *);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif //BLACKLISTFILTER_BLACKLIST_WATCHER_H
