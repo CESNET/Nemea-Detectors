ec688d9e34699f42a6c87aea562f4ed59cf7dc28
Tomas Cejka <cejkat@cesnet.cz>
2019-02-07 19:06:30 +0100

    booterfilter: removed, the functionality is provided by blacklistfilter

--- booterfilter/booterfilter-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -qO - 'https://booterblacklist.com/data/booterlist_latest.txt' |
-  sed 's/^.*$/HTTP_REQUEST_HOST == "&" ||/;
-  1s/^/:HTTP_REQUEST_HOST != "" \&\& (/; $s/ ||$/)\;/;' > /data/booter-filter/filter &&
-  pkill -USR1 -f "booter_filter "
-
